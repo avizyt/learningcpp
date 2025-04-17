81a65201b97ba2a3335233595200a1ac48efe7f1
Avijit Biswas <avijit.biswas27@outlook.com>
2025-04-17 23:46:38 +0530

    arra manipulation

--- stackAndQueue/array-manp.cpp
+++ stackAndQueue/array-manp.cpp
@@ -8,4 +8,28 @@ int main()
 {
     stack<vector<int>> st;
     queue<vector<int>> q;
+
+    mat m = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    for (int i = 0; i < m.size(); i++)
+    {
+        q.push(m[i]);
+    }
+
+    vector<int> arr;
+
+    while (!q.empty())
+    {
+        arr = q.front();
+        int s = arr.size();
+        for (int i = 0; i < s; ++i)
+        {
+            cout << arr.at(i) << " ";
+        }
+
+        q.pop();
+    }
 }
